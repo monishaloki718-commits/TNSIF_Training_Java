eda5562cfcf0634848037b5f9619d5baa81bb88e
monishaloki718-commits <monisha.loki718@gmail.com>
2026-07-20 15:04:34 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/HelloWorld/Helloworld.java
@@ -0,0 +1,11 @@
+package org.tnsif.acc.c2tc.HelloWorld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
