84549b2e4d9b0163a755a979eef670ce14eb530d
monishaloki718-commits <monisha.loki718@gmail.com>
2026-07-29 16:19:48 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/methodoverriding_super_this/ProductBottle.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.methodoverriding_super_this;
+
+public class ProductBottle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
