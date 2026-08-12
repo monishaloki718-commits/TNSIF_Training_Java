2b1790cab0bace2ee87ed33ab243595f0158c7bd
monishaloki718-commits <monisha.loki718@gmail.com>
2026-08-12 21:21:14 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/markerinterface/Student.java
@@ -0,0 +1,13 @@
+package org.tnsif.acc.c2tc.markerinterface;
+
+public class Student {
+	private int id;
+	private String name;
+	private double fees;
+	private String course;
+	
+	public void setId(int id) {
+			this.id=id;
+	}
+  
+}
