d0e9f8e85b6b7f147750dbd7858b17c1a4945dc7
monishaloki718-commits <monisha.loki718@gmail.com>
2026-08-25 22:55:47 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/arraydemo/SingleDimArray.java
@@ -0,0 +1,14 @@
+package org.tnsif.acc.c2tc.arraydemo;
+
+public class SingleDimArray {
+
+	public static void main(String[] args) {
+		int[] singlearray={1,2,3,4,5};
+		for (int i:singlearray)
+		{
+			System.out.print(i + "  ");
+		}
+
+	}
+
+}
