29b57d9196b248eed1120ab0edb084a3e555fdce
monishaloki718-commits <monisha.loki718@gmail.com>
2026-08-26 20:31:14 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/string/StringBuilderDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.acc.c2tc.string;
+
+public class StringBuilderDemo {
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder("Initial Value");
+		
+		sb.append(" -Appended"); //add appende to the end
+		sb.append(" -Again");  //add again to the end
+
+		
+		sb.insert(15, "[insert]");
+		
+		sb.replace(16 ,27,"[replaced]");
+		
+		sb.delete(2, 4);
+		
+		sb.reverse();
+		
+		System.out.println("Final stringbuilder result" +"   " + sb.toString());
+
+	}
+
+}
